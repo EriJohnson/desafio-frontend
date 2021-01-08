6d95a5c96bb79d7b824f64d661bc07a8ecd09c1b
Eri Johnson <erijohnsons.fernandes@gmail.com>
2021-01-08 08:45:03 -0300

    feat: retorna todos os eventos da API

--- agenda/src/App.js
+++ agenda/src/App.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import api from './services/api'
 
 function App() {
+  const [events, setEvents] = useState([])
+  useEffect(() => {
+    async function getEvents() {
+      try {
+        const { data } = await api.get('/events')
+        console.log('data', data)
+        setEvents(data)
+      } catch (error) {
+        alert('Ocorreu um erro ao buscar os eventos')
+      }
+    }
+    getEvents()
+  }, [])
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>Hello World</h1>
+        <ul>
+          {events.map(e => (
+            <li key={e.id}>{e.title}</li>
+          ))}
+        </ul>
       </header>
     </div>
   )
